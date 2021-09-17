0b49ff33e3e1350f1e815722ff6c08f777106a28
Thomas Gandy <thomas@gandy.info>
2021-09-17 10:30:39 +0100

    Removed Argos alias

--- .stockAliases.sh
+++ .stockAliases.sh
@@ -2,7 +2,7 @@ export stockCheckerScriptsLocation=${stockCheckerLocation}/stockCheckerScripts
 
 alias amazonXboxXStock="bash $stockCheckerScriptsLocation/amazonXboxXStock.sh"
 alias curryXboxXStock="bash $stockCheckerScriptsLocation/curryXboxXStock.sh"
-alias argosXboxXStock="bash $stockCheckerScriptsLocation/argosXboxXStock.sh"
+# alias argosXboxXStock="bash $stockCheckerScriptsLocation/argosXboxXStock.sh"
 alias veryXboxXStock="bash $stockCheckerScriptsLocation/veryXboxXStock.sh"
 alias gameXboxXStock="bash $stockCheckerScriptsLocation/gameXboxXStock.sh"
 alias emailSenderTest="bash $stockCheckerScriptsLocation/emailSenderTest.sh"
